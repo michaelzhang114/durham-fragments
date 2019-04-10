253572be300bf058f898130b32425aa4b28e93b5
Michael Zhang <sz114@duke.edu>
2019-04-10 16:12:49 -0400

    added 3 sets of 96

--- js/main.js
+++ js/main.js
@@ -4,26 +4,11 @@ var numRows = 10;
 
 
 function appendToLayer(layerNum) {
-  //<img src="/resources/img1/1.png"/>
-
-
-
   for (var j = 1; j <= numRows; j++) {
     for (var i = 1; i <= numColumns; i++) {
       $("#layer" + layerNum).append("<img class='sub"+ layerNum + "x" + (i+(j-1)*numColumns) + "' src='resources/img" + layerNum+ "/" + (i+(j-1)*numColumns) + ".PNG' height='"+ imgLength +"' width='"+ imgLength +"' style='position:absolute;top:"+ imgLength*(j-1) +"px;left:" + (i-1) * imgLength + "px'; />");
     }
   }
-
-
-  // for (var i = 1; i <= numColumns; i++) {
-  //   $("#layer" + layerNum).append("<img class='sub"+ layerNum + "x" + (i+6) + "' src='resources/img"+ layerNum+ "/" + (i+6) + ".PNG' height='"+ imgLength +"' width='"+ imgLength +"' style='position:absolute;top:"+ imgLength +"px;left:" + (i-1) * imgLength + "px'; />");
-  // }
-  // for (var i = 1; i <= numColumns; i++) {
-  //   $("#layer" + layerNum).append("<img class='sub"+ layerNum + "x" + (i+12) + "' src='resources/img" + layerNum +"/" + (i+12) + ".PNG' height='"+ imgLength +"' width='"+ imgLength +"' style='position:absolute;top:"+ imgLength*2 +"px;left:" + (i-1) * imgLength + "px'; />");
-  // }
-  // for (var i = 1; i <= numColumns; i++) {
-  //   $("#layer" + layerNum).append("<img class='sub"+ layerNum + "x" + (i+18) + "' src='resources/img" + layerNum +"/" + (i+18) + ".PNG' height='"+ imgLength +"' width='"+ imgLength +"' style='position:absolute;top:"+ imgLength*3 +"px;left:" + (i-1) * imgLength + "px'; />");
-  // }
 }
 
 function changeOpacityOfLayer(layerNum) {
